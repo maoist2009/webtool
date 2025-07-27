e1225d56360a83563b8c81cbe9c116092ea1e976
maoist2009 <179199418+maoist2009@users.noreply.github.com>
2025-07-27 13:13:00 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import {defineConfig} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+    base: '/',
+    plugins: [
+        VitePWA({
+            injectRegister: 'auto',
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            includeAssets: ['favicon.svg'],
+            manifest: {
+                name: '网络工具箱',
+                short_name: "webtool",
+                theme_color: "#667eea",
+                scope: "https",
+                start_url: "./",
+                display: "standalone",
+                background_color: "#667eea",
+                icons: [
+                    {
+                        src: "favicon.svg",
+                        sizes: "16x16 32x32 48x48 64x64 128x128 256x256 512x512",
+                        type: "image/svg+xml",
+                        purpose: "any",
+                    },
+                ],
+            },
+        })
+    ],
+});
